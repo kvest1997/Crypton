dca2ba8449ea5621869e2b486d617e238da25bb5
Kirill <kvest1997@gmail.com>
2021-07-24 20:08:51 +0700

    Добавьте файлы проекта.

---
+++ Work2/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace Work2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenBtn = new System.Windows.Forms.Button();
+            this.SaveBtn = new System.Windows.Forms.Button();
+            this.SortBtn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.MainTextBox = new System.Windows.Forms.RichTextBox();
+            this.Txt = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // OpenBtn
+            // 
+            this.OpenBtn.Location = new System.Drawing.Point(13, 13);
+            this.OpenBtn.Name = "OpenBtn";
+            this.OpenBtn.Size = new System.Drawing.Size(75, 23);
+            this.OpenBtn.TabIndex = 0;
+            this.OpenBtn.Text = "Открыть";
+            this.OpenBtn.UseVisualStyleBackColor = true;
+            this.OpenBtn.Click += new System.EventHandler(this.OpenBtn_Click);
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.Location = new System.Drawing.Point(94, 13);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(75, 23);
+            this.SaveBtn.TabIndex = 1;
+            this.SaveBtn.Text = "Сохранить";
+            this.SaveBtn.UseVisualStyleBackColor = true;
+            this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // SortBtn
+            // 
+            this.SortBtn.Location = new System.Drawing.Point(272, 13);
+            this.SortBtn.Name = "SortBtn";
+            this.SortBtn.Size = new System.Drawing.Size(102, 23);
+            this.SortBtn.TabIndex = 2;
+            this.SortBtn.Text = "Отсортировать";
+            this.SortBtn.UseVisualStyleBackColor = true;
+            this.SortBtn.Click += new System.EventHandler(this.SortBtn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // MainTextBox
+            // 
+            this.MainTextBox.DetectUrls = false;
+            this.MainTextBox.Location = new System.Drawing.Point(13, 88);
+            this.MainTextBox.Name = "MainTextBox";
+            this.MainTextBox.ReadOnly = true;
+            this.MainTextBox.Size = new System.Drawing.Size(361, 303);
+            this.MainTextBox.TabIndex = 3;
+            this.MainTextBox.Text = "";
+            this.MainTextBox.WordWrap = false;
+            this.MainTextBox.TextChanged += new System.EventHandler(this.MainTextBox_TextChanged);
+            // 
+            // Txt
+            // 
+            this.Txt.AutoSize = true;
+            this.Txt.Location = new System.Drawing.Point(13, 67);
+            this.Txt.Name = "Txt";
+            this.Txt.Size = new System.Drawing.Size(0, 15);
+            this.Txt.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 422);
+            this.Controls.Add(this.Txt);
+            this.Controls.Add(this.MainTextBox);
+            this.Controls.Add(this.SortBtn);
+            this.Controls.Add(this.SaveBtn);
+            this.Controls.Add(this.OpenBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Парсер TXT";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenBtn;
+        private System.Windows.Forms.Button SaveBtn;
+        private System.Windows.Forms.Button SortBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox MainTextBox;
+        private System.Windows.Forms.Label Txt;
+    }
+}
+
---
+++ Work2/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Work2
+{
+    public partial class Form1 : Form
+    {
+        bool f_open, f_save; // определяют выбор файла и его сохранение
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            f_open = false;
+            f_save = false;
+            Txt.Text = "Ничего не открыто";
+            MainTextBox.Text = "";
+        }
+
+        private void OpenBtn_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Txt.Text = openFileDialog1.FileName;
+
+                f_open = true;
+                f_save = false;
+
+                MainTextBox.Clear();
+
+                StreamReader streamReader = File.OpenText(openFileDialog1.FileName);
+
+                string line = null;
+                line = streamReader.ReadLine();
+
+                while(line!=null)
+                {
+                    MainTextBox.AppendText(line);
+                    MainTextBox.AppendText("\r\n");
+                    line = streamReader.ReadLine();
+                }
+
+                streamReader.Close();
+            }
+        }
+
+        private void SortBtn_Click(object sender, EventArgs e)
+        {
+            if (!f_open) return;
+
+            if (f_save) return;
+
+            
+            List<string> name = new List<string>();
+            List<int> inHour = new List<int>();
+            List<int> kolvHour = new List<int>();
+            List<string> resultLine = new List<string>();
+            StreamReader streamReader;
+
+
+           using (streamReader = File.OpenText(Txt.Text))
+           {
+                string tempLine = streamReader.ReadLine();
+
+                while (tempLine != null)
+                {
+                    string[] line = tempLine.Split(" ");
+
+                    name.Add(line[0]);
+                    inHour.Add(Convert.ToInt32(line[1]));
+                    kolvHour.Add(Convert.ToInt32(line[2]));
+
+                    tempLine = streamReader.ReadLine();
+                }
+            }
+
+            MainTextBox.Clear();
+
+
+            for (int i = 0; i < name.Count(); i++)
+            {
+                string currentName = name[i];
+                int tempInHour = inHour[i];
+                int tempKolvHour = kolvHour[i];
+                int sumHour = 0;
+                bool replace = false;
+
+                for (int j = 0; j < name.Count(); j++)
+                {
+                    if (currentName == name[j] && i != j)
+                    {
+                        tempInHour += inHour[j];
+                        tempKolvHour += kolvHour[j];
+                    }
+
+                    if (currentName == name[j] && i > j)
+                        replace = true;
+                }
+                sumHour = tempInHour * tempKolvHour;
+                if (!replace)
+                {
+                    resultLine.Add(currentName + " " + tempInHour + " " + tempKolvHour);
+                }
+            }
+
+
+            for (int i = 0; i < resultLine.Count(); i++)
+            {
+                for (int k = 0; k < resultLine.Count()-1; k++)
+                {
+                    if(needToReOrder(resultLine[k], resultLine[k+1]))
+                    {
+                        string tempStr = resultLine[k];
+                        resultLine[k] = resultLine[k + 1];
+                        resultLine[k + 1] = tempStr;
+                    }
+                }
+                MainTextBox.AppendText(resultLine[i]);
+                MainTextBox.AppendText("\r\n");
+            }
+
+            streamReader.Close();
+        }
+
+        private static bool needToReOrder(string s1, string s2)
+        {
+            for (int i = 0; i < (s1.Length > s2.Length ? s2.Length : s1.Length); i++)
+            {
+                if (s1.ToCharArray()[i] < s2.ToCharArray()[i]) return false;
+                if (s1.ToCharArray()[i] > s2.ToCharArray()[i]) return true;
+            }
+            return false;
+        }
+
+        private void MainTextBox_TextChanged(object sender, EventArgs e)
+        {
+            f_save = false;
+        }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            if (!f_open) return;
+
+            if (f_save) return;
+
+            StreamWriter streamWriter = File.CreateText(openFileDialog1.FileName);
+            string line;
+            for (int i = 0; i < MainTextBox.Lines.Length; i++)
+            {
+                line = MainTextBox.Lines[i].ToString();
+
+                streamWriter.WriteLine(line);
+            }
+
+            streamWriter.Close();
+
+            MessageBox.Show("Файл сохранен","", MessageBoxButtons.OK);
+        }
+    }
+}
